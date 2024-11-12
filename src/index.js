7151a47f37d848404b20e085281a25b403cdfb4a
Daniel <mihhail.kakourov@ivkhk.ee>
2024-11-12 14:08:16 +0200

    redux2

--- src/index.js
+++ src/index.js
@@ -1,54 +1,33 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import {createStore} from "redux"
-
-const initianState = {value:0}
-
-const reducer = (state = initianState, action) =>{
-    switch (action.type) {
-        case "INC":
-            return{
-                ...state,
-                value: state.value +1
-            }
-        case "DEC":
-            return{
-                ...state,
-                value: state.value - 1
-            }
-        case "RND":
-            return{
-                ...state,
-                value: state.value * action.payload
-            }
-        default:
-            return state;
-    }
-}
+import {bindActionCreators, createStore} from "redux"
+import reducer from "./reducer"
+import * as actions from "./actions"
 
 const store = createStore(reducer)
+
+const {dispatch, subscribe, getState} = store
 
 const update = () => {
     document.getElementById("counter").textContent = store.getState().value
 }
 
-store.subscribe(update)
+subscribe(update)
 
-const inc = () => ({type:"INC"})
-const dec = () => ({type:"DEC"})
-const rnd = (value) => ({type:"RND", payload: value})
+const {inc, dec, inc5, dec5, rnd} = bindActionCreators( actions, dispatch)
 
-document.getElementById("inc").addEventListener("click", () => {
-    store.dispatch(inc())
-})
 
-document.getElementById("dec").addEventListener("click", () => {
-    store.dispatch(dec())
-})
+document.getElementById("inc").addEventListener("click", inc)
+
+document.getElementById("dec").addEventListener("click", dec)
+
+document.getElementById("inc5").addEventListener("click", inc5)
+
+document.getElementById("dec5").addEventListener("click", dec5)
 
 document.getElementById("rnd").addEventListener("click", () => {
     const value = Math.floor(Math.random() * 10)
-    store.dispatch(rnd(value))
+    rnd(value)
 })
 
 ReactDOM.render(
